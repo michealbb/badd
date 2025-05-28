820f09a95bded3e9e6d637f73064fb9cf9829c25
michealbb <michealbourne91@gmail.com>
2025-05-28 15:23:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import "dotenv/config";
 import app from "./api/server.js";
-import { SERVER_PORT } from "./api/services/helpers/environment.js";
 
-
-app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}...`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
